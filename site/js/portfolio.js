2e4d97a6341d5ebf7eaf3e529243cd8b501ecc9f
MBoussel <mb.boussel@gmail.com>
2025-03-31 13:06:07 +0200

    finish JS step3 in part3

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -9,9 +9,15 @@ avatar.addEventListener("click", () => {
 
 const buttonName = document.querySelector(".buttonname");
 const name = document.querySelector("#firstname");
+const pinkBackground = document.querySelector(".pink-bg");
 buttonName.addEventListener("click", () => {
     const newName = prompt("Enter your name");
     if (newName) {
       name.textContent = newName;
       name.style.color = "white";
+      const newColor = prompt("Enter a color");
+      if (newColor) {
+        pinkBackground.style.backgroundColor = newColor;
+      }
     }});
+    
