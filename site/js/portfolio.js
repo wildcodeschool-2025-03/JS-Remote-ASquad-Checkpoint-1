cc98da85c56fadbe5dc97614d5ad475fbf10a330
anacslr <anais.cieslar@gmail.com>
2025-03-31 12:20:30 +0200

    finish JS step 1 in part 2

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+const avatarImg = document.querySelector(".avatar");
+const avatarImg2 = document.querySelector(".avatar2");
+
+
+avatarImg.addEventListener("click", () => {
+    avatarImg.style.display = "none";
+    avatarImg2.style.display = "block";
+
+})
