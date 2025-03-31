c5224d1e1b8747d424ef0905324845231008a9f9
anacslr <anais.cieslar@gmail.com>
2025-03-31 12:51:39 +0200

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,9 +1,18 @@
 const avatarImg = document.querySelector(".avatar");
 const avatarImg2 = document.querySelector(".avatar2");
-
+const pinkBg = document.querySelector(".description pink-bg");
+const btn = document.querySelector(".btn");
+let firstname = document.querySelector("#firstname");
 
 avatarImg.addEventListener("click", () => {
     avatarImg.style.display = "none";
     avatarImg2.style.display = "block";
+
+})
+
+btn.addEventListener("click", () => {
+    let userName = prompt("What's your name");
+    firstname.style.color = "white";
+    firstname.innerHTML = `${userName}`;
 
 })
