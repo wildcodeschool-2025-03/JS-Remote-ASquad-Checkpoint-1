6df254c71dcd8945c653b172e0842ab5f88897fc
marina saraiva <marinasaraiva@hotmail.fr>
2025-03-31 13:55:00 +0200

    code plus propre

--- site/index.js
+++ site/index.js
@@ -4,14 +4,14 @@ console.log("hello");
 // changer l'avatar
 const avatar = document.querySelector(".avatar img"); 
 
-avatar.addEventListener("click", function () {
+avatar.addEventListener("click", () => {
     avatar.src = "image/avatar.svg";
     avatar.alt = "new avatar";
 });
 
 // changer le nom Matt et le click
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
    
     const changeBtn = document.getElementById("changeColorBtn");
     const firstname = document.getElementById("firstname");
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // changer les couleurs côté utilisateur avec le bouton
-    changeColorBtn.addEventListener("click", function () {
+    changeColorBtn.addEventListener("click", () => {
         const newBackground = prompt("Please enter a background color:");
 
         if (newBackground) {
@@ -53,40 +53,34 @@ document.addEventListener("DOMContentLoaded", function () {
             alert("Please enter a valid name.");
         }
 });    
-});
-
 
 // changer la couleur des liens html
-document.addEventListener("DOMContentLoaded", function () {
     const lienNav = document.querySelectorAll(".menu a");
 
     lienNav.forEach(function(link) {
         link.style.color = "#750ff7";
     });
-});
+ 
 
 
 // modifier les Items list dans le li de Front-end
-
-document.addEventListener("DOMContentLoaded", function () {
-
     const modifyListBtn = document.getElementById("modifyList");
     const listItems = document.querySelectorAll("#front-dev-tools li");
 
     const elementList = ["VSCode", "Github", "Terminal"];
 
-    modifyListBtn.addEventListener("click", function () {
+    modifyListBtn.addEventListener("click", () => {
         for (let i = 0; i < listItems.length; i++) {
             if (elementList[i]) {
                 listItems[i].textContent = elementList[i];
             }
         }
     });
-});
 
 
-// ajouter un ou plusieurs element(s) dans le li de back-end
-document.addEventListener("DOMContentLoaded", function () {
+
+
+// modifier les Items list dans le li de Front-end
     const formTolls = document.getElementById("formTolls");
     const tollsElement = document.getElementById("tollsElement");
 
@@ -108,6 +102,34 @@ document.addEventListener("DOMContentLoaded", function () {
             backElement.value = "";
         }     
     });
+
+
+    
 });
+
+
+// ajouter un ou plusieurs element(s) dans le li de back-end
+    const formTolls = document.getElementById("formTolls");
+    const tollsElement = document.getElementById("tollsElement");
+
+    formTolls.addEventListener("submit", function (event) {
+        event.preventDefault();
+
+        const backElement = document.getElementById("backElement");
+        const newBackElement = backElement.value;
+
+        if (newBackElement) {
+            // creer un nouvel li
+            const liTools = document.createElement("li");
+            liTools.innerHTML = newBackElement;
+
+            // ajoute a la liste
+            tollsElement.appendChild(liTools);
+
+            // reinitialise 
+            backElement.value = "";
+        }     
+    });
+
 
 
