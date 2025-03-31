f9dc09831c79ddec26a873956b6b2df53e26e015
MBoussel <mb.boussel@gmail.com>
2025-03-31 09:46:42 +0200

    finish algo1 in part 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -6,8 +6,11 @@ thirdAngle(90, 30) doit retourner 60
 thirdAngle(20, 80) doit retourner 80
 */
 
-function thirdAngle(a, b) {
   // Your code here !
+  const triangle = 180;
+function thirdAngle(a, b) {
+  return triangle - (a + b);
 }
 
 module.exports = thirdAngle;
+
