6989a47436d40f792b714351b40af0cad717389d
MBoussel <mb.boussel@gmail.com>
2025-03-31 12:06:23 +0200

    finish JS step1 in part3

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+const avatar = document.querySelector(".avatar-bis");
+
+
+avatar.addEventListener("click", () => {
+   const img = document.querySelector(".avatar-bis");
+    img.src="/site/image/avatar.svg";
+   
+});
