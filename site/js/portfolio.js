23531779ba7ab1b5a3632c478ff98027e5ff5a28
NabilY-5 <nabilyahya59280@gmail.com>
2025-03-31 13:04:39 +0200

    finish JS step4 in part2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -10,7 +10,13 @@ avatar.addEventListener("click", () => {
 
 const button = document.querySelector("#button");
 const name = document.querySelector("#firstname");
-const pinkBackground = document.querySelector(".pink-bg");
+// const pinkBackground = document.querySelector(".pink-bg"); Step 3 JS
+
+// Step 4 JS
+const elementsPink = document.querySelectorAll(
+  ".pink-bg, .description pink-bg"
+);
+const textPink = document.querySelectorAll("h4");
 
 button.addEventListener("click", () => {
   const newName = prompt("Enter your name");
@@ -19,11 +25,18 @@ button.addEventListener("click", () => {
     name.textContent = newName;
     name.style.color = "white";
 
-    // Step 3 JS
+    // Step 3 JS + Step 4
 
     const newColor = prompt("Enter a color");
     if (newColor) {
-      pinkBackground.style.backgroundColor = newColor;
+      //   pinkBackground.style.backgroundColor = newColor;  Step 3 JS
+
+      elementsPink.forEach((element) => {
+        element.style.backgroundColor = newColor;
+      });
+      textPink.forEach((h4) => {
+        h4.style.color = newColor;
+      });
     }
   }
 });
