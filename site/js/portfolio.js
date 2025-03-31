e0edff640b3b7e61272eb373176909e0a9b3c87b
SPerrierPro <sophie.perrier.pro@gmail.com>
2025-03-31 11:28:28 +0200

    Finish step 2 in JS part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,15 +1,17 @@
 const avatar = document.querySelector(".avatar");
-console.log(avatar.src);
-
-/* avatar.onclick = function () {
-    if (avatar.src == "image/avatar.svg") {
-        avatar.src = "image/avatar-bis.png";
-    } else {
-        avatar.src = "image/avatar.svg";
-    }
-}
-*/
 
 avatar.onclick = function () {
-    avatar.src = "image/avatar.svg";
+    if (avatar.src == "http://127.0.0.1:5500/JS-Remote-ASquad-Checkpoint-1/site/image/avatar-bis.png") {
+        avatar.src = "image/avatar.svg";
+    } else {
+        avatar.src = "image/avatar-bis.png";
+    }
 }
+
+const buttonChangeName = document.querySelector("button");
+const firstName = document.getElementById("firstname");
+
+buttonChangeName.addEventListener('click', function() {
+    firstName.textContent = prompt("Enter your name: ");
+    firstName.style.color = "white";
+})
