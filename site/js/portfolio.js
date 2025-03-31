08303a48a2656b51fc05600ba0ea302a1900c05b
GARY GRAS <gary.gras07@gmail.com>
2025-03-31 13:32:10 +0200

    finish js step2 part2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -4,3 +4,15 @@ imageAv.addEventListener('click', function () {
     imageAv.src = "image/avatar-bis.png";
   });
   
+  const changeNames = document.querySelector("#firstname");
+  
+  changeNames.addEventListener('click', function() {
+    const userName = prompt("Please enter your name:");
+  
+    if (userName) {
+      changeNames.textContent = userName; 
+      changeNames.style.color = "white"; 
+    }
+  });
+
+
