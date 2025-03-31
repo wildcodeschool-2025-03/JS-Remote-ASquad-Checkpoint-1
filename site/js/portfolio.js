6cff10134173a9a71fb6d5aa826dfe68c3d515b2
anacslr <anais.cieslar@gmail.com>
2025-03-31 14:01:14 +0200

    finish JS step 4 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,8 +1,11 @@
 const avatarImg = document.querySelector(".avatar");
 const avatarImg2 = document.querySelector(".avatar2");
 const pinkBg = document.querySelector(".pink-bg");
+const pinkBgAll = document.querySelectorAll(".pink-bg");
 const btn = document.querySelector(".btn");
 let firstname = document.querySelector("#firstname");
+let pinkText = document.querySelector(".pink-text");
+let pinkTextAll = document.querySelectorAll(".pink-text");
 
 avatarImg.addEventListener("click", () => {
     avatarImg.style.display = "none";
@@ -12,9 +15,9 @@ avatarImg.addEventListener("click", () => {
 
 btn.addEventListener("click", () => {
     let color = prompt("Enter a color");
-    pinkBg.style.backgroundColor = `${color}`;
-    console.log(color);
-
+    pinkBgAll.forEach((pink) => pink.style.backgroundColor = `${color}`);
+    pinkTextAll.forEach((txt) => txt.style.color = `${color}`);
+   
     let userName = prompt("Enter your name");
     firstname.style.color = "white";
     firstname.innerHTML = `${userName}`;
