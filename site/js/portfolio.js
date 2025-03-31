9c92683f29d912305914e201988665b186cf8159
K-Smagghe <kevin.smagghe.pro@gmail.com>
2025-03-31 11:56:51 +0200

    finish js step 1 in part1

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+/* changer l'image de l'avatar quand je clique dessus */
+
+const avatar = document.querySelector(".avatarJs")
+avatar.addEventListener("click",() => {
+avatar.src="image/avatar.svg";
+});
