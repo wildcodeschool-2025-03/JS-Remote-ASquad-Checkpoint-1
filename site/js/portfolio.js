658a57b5903c205e1dd7e73bd1190a31360457e0
NabilY-5 <nabilyahya59280@gmail.com>
2025-03-31 12:12:21 +0200

    finish JS step2 in part2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,3 +5,17 @@ const avatar = document.querySelector("#avatar");
 avatar.addEventListener("click", () => {
   avatar.src = "image/avatar.svg";
 });
+
+// Step 2 JS
+
+const button = document.querySelector("#button");
+const name = document.querySelector("#firstname");
+
+button.addEventListener("click", () => {
+  const newName = prompt("Enter your name");
+
+  if (newName) {
+    name.textContent = newName;
+    name.style.color = "white";
+  }
+});
